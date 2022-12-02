3c5c8425001c241f704e24bd276968a923857867
Evan Haveman <ehaveman@datameer.com>
2022-12-02 00:39:33 -0800

    cleanup

--- 02_rock_paper_scissors/part1.exs
+++ 02_rock_paper_scissors/part1.exs
@@ -1,6 +1,16 @@
 #!/usr/bin/env elixir
 
-scoring = %{ "A X" => 1+3, "A Y" => 2+6, "A Z" => 3+0, "B X" => 1+0, "B Y" => 2+3, "B Z" => 3+6, "C X" => 1+6, "C Y" => 2+0, "C Z" => 3+3 }
+scoring = %{ 
+	"A X" => 1+3, 
+	"A Y" => 2+6, 
+	"A Z" => 3+0, 
+	"B X" => 1+0, 
+	"B Y" => 2+3, 
+	"B Z" => 3+6, 
+	"C X" => 1+6, 
+	"C Y" => 2+0, 
+	"C Z" => 3+3 
+}
 
 IO.read(:stdio, :all) 
 	|> String.split("\n", trim: true) 
